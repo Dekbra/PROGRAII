f45ea57805d1bbbde60fbc15a289b94e16f87035
dekbra <ditho.guevara04@gmail.com>
2024-04-02 20:37:15 -0600

    ccalculadora

--- src/main/java/com/example/programacion2/MainActivity.java
+++ src/main/java/com/example/programacion2/MainActivity.java
@@ -1,24 +1,92 @@
 package com.example.programacion2;
 
-import android.os.Bundle;
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.content.FileProvider;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tempVal;
+    Button btn;
+    RadioGroup opt;
+    Spinner spn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btn = findViewById(R.id.btnCalcular);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tempVal = findViewById(R.id.txtnum1);
+                double num1 = Double.parseDouble(tempVal.getText().toString());
+                tempVal = findViewById(R.id.txtnum2);
+                double num2 = Double.parseDouble(tempVal.getText().toString());
+
+                double respuesta = 0;
+                spn = findViewById(R.id.spnOpciones);
+                int factorial = 1;
+
+                switch (spn.getSelectedItemPosition()) {
+                    case 0:
+                        respuesta = num1 + num2;
+                        break;
+                    case 1:
+                        respuesta = num1 - num2;
+                        break;
+                    case 2:
+                        respuesta = num1 * num2;
+                        break;
+                    case 3:
+                        respuesta = num1 / num2;
+                        break;
+                    case 4:
+                        respuesta = (num1*num2)/100;
+                        break;
+                    case 5:
+                        respuesta = pow(num1,num2);
+                        break;
+                    case 6:
+                        for (int i=2;i<=num1;i++){
+                            factorial *=i;
+                            respuesta= factorial;
+                        }
+
+                        break;
+                    case 7:
+                        if (num1 >= 0 && num2 > 0) {
+                            respuesta = Math.pow(num1, 1 / num2);
+                            break;
+                        }
+                }
+                tempVal = findViewById(R.id.lblrespuesta);
+                tempVal.setText("Respuesta: " + respuesta);
+            }
         });
     }
 }

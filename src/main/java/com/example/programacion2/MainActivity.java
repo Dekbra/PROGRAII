c4804ddd00d7f204228404b53ed279807023e70a
dekbra <ditho.guevara04@gmail.com>
2024-04-02 21:38:46 -0600

    sensor_acelerometro

--- src/main/java/com/example/programacion2/MainActivity.java
+++ src/main/java/com/example/programacion2/MainActivity.java
@@ -3,37 +3,36 @@ package com.example.programacion2;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-import android.graphics.Color;
+
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.RadioGroup;
-import android.widget.Spinner;
+
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
 
 
-    TextView tempVal;
     SensorManager sensorManager;
     Sensor sensor;
     SensorEventListener sensorEventListener;
+    TextView tempVal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tempVal = findViewById(R.id.lblSensorProximidad);
-        activarSensorProximidad();
+        tempVal = findViewById(R.id.lblSensorAcelerometro);
+        activarSensorAcelerometro();
+
     }
     @Override
     protected void onResume() {
-        iniciar();
+        inicar();
         super.onResume();
     }
     @Override
@@ -41,25 +40,18 @@ public class MainActivity extends AppCompatActivity {
         detener();
         super.onPause();
     }
-    private void activarSensorProximidad(){
+    private void activarSensorAcelerometro(){
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         if(sensor==null){
-            tempVal.setText("Tu telefono NO tiene sensor de proximidad");
+            tempVal.setText("Tu dispositivo NO cuenta con el sensor acelerometro.");
             finish();
         }
         sensorEventListener = new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent sensorEvent) {
-                double valor = sensorEvent.values[0];
-                tempVal.setText("Proximidad: "+ valor);
-                if( valor<=4 ){
-                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
-                } else if (valor<=8) {
-                    getWindow().getDecorView().setBackgroundColor(Color.RED);
-                }else{
-                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
-                }
+                tempVal.setText("Acelerometro: X="+ sensorEvent.values[0]+ "; Y="+ sensorEvent.values[1] +"; Z="+
+                        sensorEvent.values[2]);
             }
             @Override
             public void onAccuracyChanged(Sensor sensor, int i) {
@@ -67,8 +59,8 @@ public class MainActivity extends AppCompatActivity {
             }
         };
     }
-    private void iniciar(){
-        sensorManager.registerListener(sensorEventListener, sensor, 2000*1000);
+    private void inicar(){
+        sensorManager.registerListener(sensorEventListener, sensor,2000*1000 );
     }
     private void detener(){
         sensorManager.unregisterListener(sensorEventListener);

7662ef4b57b1ffad424d7ae40cc21113efcf0cc2
dekbra <ditho.guevara04@gmail.com>
2024-04-02 21:31:17 -0600

    SLITEAMIGOS

--- src/main/java/com/example/programacion2/MainActivity.java
+++ src/main/java/com/example/programacion2/MainActivity.java
@@ -2,67 +2,107 @@ package com.example.programacion2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
-
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
-
-
-    SensorManager sensorManager;
-    Sensor sensor;
-    SensorEventListener sensorEventListener;
+    Button btn;
+    FloatingActionButton fab;
     TextView tempVal;
-
+    String accion = "nuevo";
+    String id="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tempVal = findViewById(R.id.lblSensorAcelerometro);
-        activarSensorAcelerometro();
 
+        fab = findViewById(R.id.fabRegresarLtsAmigos);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirActividad();
+            }
+        });
+        btn = findViewById(R.id.btnGuardarAgendaAmigos);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    tempVal = findViewById(R.id.txtNombre);
+                    String nombre = tempVal.getText().toString();
+
+                    tempVal = findViewById(R.id.txtDireccion);
+                    String direccion = tempVal.getText().toString();
+
+                    tempVal = findViewById(R.id.txtTelefono);
+                    String tel = tempVal.getText().toString();
+
+                    tempVal = findViewById(R.id.txtEmail);
+                    String email = tempVal.getText().toString();
+
+                    tempVal = findViewById(R.id.txtDui);
+                    String dui = tempVal.getText().toString();
+
+                    DB db = new DB(getApplicationContext(), "",null, 1);
+                    String[] datos = new String[]{id,nombre,direccion,tel,email,dui};
+                    mostrarMsg(accion);
+                    String respuesta = db.administrar_amigos(accion, datos);
+                    if(respuesta.equals("ok")){
+                        Toast.makeText(getApplicationContext(), "Amigo guardado con exito", Toast.LENGTH_LONG).show();
+                        abrirActividad();
+                    }else{
+                        Toast.makeText(getApplicationContext(), "Error al intentar guardar el amigo: "+ respuesta, Toast.LENGTH_LONG).show();
+                    }
+                }catch (Exception e){
+                    Toast.makeText(getApplicationContext(), "Error: "+ e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        mostrarDatosAmigos();
     }
-    @Override
-    protected void onResume() {
-        inicar();
-        super.onResume();
-    }
-    @Override
-    protected void onPause() {
-        detener();
-        super.onPause();
-    }
-    private void activarSensorAcelerometro(){
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        if(sensor==null){
-            tempVal.setText("Tu dispositivo NO cuenta con el sensor acelerometro.");
-            finish();
+    private void mostrarDatosAmigos(){
+        try{
+            Bundle parametros = getIntent().getExtras();//Recibir los parametros...
+            accion = parametros.getString("accion");
+
+            if(accion.equals("modificar")){
+                String[] amigos = parametros.getStringArray("amigos");
+                id = amigos[0];
+
+                tempVal = findViewById(R.id.txtNombre);
+                tempVal.setText(amigos[1]);
+
+                tempVal = findViewById(R.id.txtDireccion);
+                tempVal.setText(amigos[2]);
+
+                tempVal = findViewById(R.id.txtTelefono);
+                tempVal.setText(amigos[3]);
+
+                tempVal = findViewById(R.id.txtEmail);
+                tempVal.setText(amigos[4]);
+
+                tempVal = findViewById(R.id.txtDui);
+                tempVal.setText(amigos[5]);
+            }
+        }catch (Exception e){
+            mostrarMsg("Error al mostrar datos: "+ e.getMessage());
         }
-        sensorEventListener = new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent sensorEvent) {
-                tempVal.setText("Acelerometro: X="+ sensorEvent.values[0]+ "; Y="+ sensorEvent.values[1] +"; Z="+
-                        sensorEvent.values[2]);
-            }
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int i) {
-
-            }
-        };
     }
-    private void inicar(){
-        sensorManager.registerListener(sensorEventListener, sensor,2000*1000 );
+    private void mostrarMsg(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
     }
-    private void detener(){
-        sensorManager.unregisterListener(sensorEventListener);
+    private void abrirActividad(){
+        Intent abrirActividad = new Intent(getApplicationContext(), listado_amigos.class);
+        startActivity(abrirActividad);
     }
 }
